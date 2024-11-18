6ad46252fc088cbd7f5b04f2c83a988513d91f57
Razewang <47569570+Razewang@users.noreply.github.com>
2024-11-18 21:12:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.momb.top": dockerHub,
+  "quay.momb.top": "https://quay.io",
+  "gcr.momb.top": "https://gcr.io",
+  "k8s-gcr.momb.top": "https://k8s.gcr.io",
+  "k8s.momb.top": "https://registry.k8s.io",
+  "ghcr.momb.top": "https://ghcr.io",
+  "cloudsmith.momb.top": "https://docker.cloudsmith.io",
+  "ecr.momb.top": "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker-staging.momb.top": dockerHub,
 };
 
 function routeByHosts(host) {
